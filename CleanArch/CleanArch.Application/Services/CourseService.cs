62501a432952a8387a75b64cdd3f06e8b9ae83f0
saeedsaedvand <saeed_saedvand@live.com>
2020-12-18 06:50:58 -0800

    Commit Implementaion of Service and Repository GetCourses Methods

---
+++ CleanArch/CleanArch.Application/Services/CourseService.cs
@@ -0,0 +1,26 @@
+﻿using CleanArch.Application.Interfaces;
+using CleanArch.Application.ViewModels;
+using CleanArch.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanArch.Application.Services
+{
+    public class CourseService : ICourseService
+    {
+        private readonly ICourseRepository _repository;
+        public CourseService(ICourseRepository repository)
+        {
+            _repository = repository;
+        }
+        public CourseViewModel GetCourses()
+        {
+            return new CourseViewModel
+            {
+                Courses = _repository.GetCourses()
+            };
+
+        }
+    }
+}
---
+++ CleanArch/CleanArch.Infra.Data/Repositories/CourseRepository.cs
@@ -0,0 +1,22 @@
+﻿using CleanArch.Domain.Entities;
+using CleanArch.Domain.Interfaces;
+using CleanArch.Infra.Data.Context;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanArch.Infra.Data.Repositories
+{
+    public class CourseRepository : ICourseRepository
+    {
+        private readonly UniversityDBContext _context;
+        public CourseRepository(UniversityDBContext context)
+        {
+            _context = context;
+        }
+        public IEnumerable<Course> GetCourses()
+        {
+            return _context.Courses;
+        }
+    }
+}
