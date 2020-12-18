0aef99f26cdecce34873768faff9af99c9a86262
saeedsaedvand <saeed_saedvand@live.com>
2020-12-18 05:56:46 -0800

    Initial Commit of Infrastructure.Data Project, Update MVC for new UniversityDBContext

---
+++ CleanArch/CleanArch.Infra.Data/Context/UniversityDBContext.cs
@@ -0,0 +1,18 @@
+﻿using CleanArch.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CleanArch.Infra.Data.Context
+{
+    //Installing Microsoft.EntityFrameworkCore
+    //Installing Microsoft.EntityFrameworkCore.Design
+    //Installing Microsoft.EntityFrameworkCore.SqlServer
+    //Installing Microsoft.EntityFrameworkCore.Tools
+    public class UniversityDBContext : DbContext
+    {
+        public UniversityDBContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Course> Courses { get; set; }
+    }
+}
