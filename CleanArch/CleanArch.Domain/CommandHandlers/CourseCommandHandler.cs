548de19fac7f7d4cd73ce501aebf8e6fd1fa68a7
saeedsaedvand <saeed_saedvand@live.com>
2020-12-25 06:18:04 -0800

    Commands, CommandHandlers, Updated CourseRepository, CleanedUp IoC Container

---
+++ CleanArch/CleanArch.Domain/CommandHandlers/CourseCommandHandler.cs
@@ -0,0 +1,32 @@
+﻿using CleanArch.Domain.Commands;
+using CleanArch.Domain.Entities;
+using CleanArch.Domain.Interfaces;
+using MediatR;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CleanArch.Domain.CommandHandlers
+{
+    public class CourseCommandHandler : IRequestHandler<CreateCourseCommand, bool>
+    {
+        private readonly ICourseRepository _courseRepository;
+
+        public CourseCommandHandler(ICourseRepository courseRepository)
+        {
+            _courseRepository = courseRepository;
+        }
+        Task<bool> IRequestHandler<CreateCourseCommand, bool>.Handle(CreateCourseCommand request, CancellationToken cancellationToken)
+        {
+            var course = new Course
+            {
+                Name = request.Name,
+                Description = request.Description,
+                ImageUrl = request.ImageUrl
+            };
+            _courseRepository.Add(course);
+
+            return Task.FromResult(true);
+        }
+    }
+}
